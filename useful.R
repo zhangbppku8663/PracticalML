d60f1d301784e3a15b9ae0f9d66f699dadd33ba1
Eric <zhangbppku8663@gmail.com>
2017-07-27 02:29:12 -0400

    updated figures folder and new CV method

--- useful.R
+++
@@ -1,8 +0,0 @@
-useful <- function(dataframe) {
-  useful_names <- c()
-  for (name in names(dataframe){
-    if (sum(is.na(training[,name])) > 100) {next}
-    else {useful <- c(useful_names, name)}
-  }
-  useful_names
-}
